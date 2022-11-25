b3880b21625d7c39e38d9d8999c567e63c86c54e
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 20:50:45 +0300

    Task_1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите два числа для сравнения:");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if(a == b)
+{
+    Console.WriteLine("Числа равны. Перезапустите программу и введети разные числа.");
+}
+else
+{
+    if(a > b)
+    {
+        max = a;
+        Console.Write("Первое число ");
+        Console.Write(max);
+        Console.Write(" больше второго числа ");
+        Console.Write(b);
+    }
+    else
+    {
+        max = b;
+        Console.Write("Второе число ");
+        Console.Write(max);
+        Console.Write(" больше первого числа ");
+        Console.Write(a);
+    }
+}

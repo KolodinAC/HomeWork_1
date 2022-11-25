8625ade5eedbf0a6da5047cf42f1665ae2dec796
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 22:15:53 +0300

    Task_4 done

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите желаемое число:");
+
+int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+while(i < N)
+{
+    i += 2;
+    Console.Write(i);
+    Console.Write(" ");
+}
